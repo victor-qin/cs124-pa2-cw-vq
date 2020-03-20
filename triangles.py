c51afbb230f515d64690062023df6876f5e00299
Carolyn Wong <carolynwong@college.harvard.edu>
2020-03-20 16:17:43 -0400

    implementation of triangles task

---
+++ triangles.py
@@ -0,0 +1,36 @@
+from random import choices
+from strassen import strassen
+
+
+def graphGen(dim, p):
+    """ Generates graph with dim vertices, edge probability p. """
+    matrix = [choices([0, 1], [1-p, p], k=dim)
+              for i in range(dim)]
+    return matrix
+
+
+def triangles(dim, p):
+    """ Computes number of triangles in a graph using Strassen's algorithm. """
+    graph = graphGen(dim, p)
+    cubedGraph = strassen(strassen(graph, graph), graph)
+    diagSum = 0
+    # Add diagonal entries
+    for i in range(dim):
+        diagSum += cubedGraph[i][i]
+    numTriangles = diagSum / 6
+    return numTriangles
+
+
+if __name__ == "__main__":
+    # Edit dims, probabilities, numTrials here
+    dim = 1024
+    probs = [0.01]
+    numTrials = 1
+
+    # Compute average number of triangles for given parameters
+    for p in probs:
+        avg = 0
+        for i in range(numTrials):
+            avg += triangles(dim, p)
+        avg = avg / numTrials
+        print("Average number of triangles, probability %f: %f" % (p, avg))
