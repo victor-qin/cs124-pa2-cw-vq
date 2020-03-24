069c7c398575f98cfc4870812c3d67564c24a27a
Carolyn Wong <carolynwong@college.harvard.edu>
2020-03-23 22:48:31 -0400

    complete implementation of triangles, graphGen

--- c++/triangles.cpp
+++ c++/triangles.cpp
@@ -12,16 +12,24 @@ void graphGen(vector<int> &matrix, int dim, float p)
     // Generates graph with dim vertices, edge probability p.
 
     // Define random number generator
-    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_int_distribution<int> distribution(0, 1);
+    // unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    // std::default_random_engine generator(seed);
+    std::default_random_engine generator;
+    std::bernoulli_distribution distribution(p);
 
     // Fill in random values for matrices
     for (int i = 0; i < dim; i++)
     {
         for (int j = 0; j < dim; j++)
         {
-            matrix[i * dim + j] = distribution(generator);
+            if (i == j)
+            {
+                matrix[i * dim + j] = 0;
+            }
+            else
+            {
+                matrix[i * dim + j] = distribution(generator);
+            }
         }
     }
     return;
@@ -30,6 +38,7 @@ void graphGen(vector<int> &matrix, int dim, float p)
 double triangles(vector<int> &matrix, vector<int> &output, int dim, float p)
 {
     // Computes number of triangles in a graph using Strassen's algorithm.
+    const double divFactor = 6.0;
 
     // Generate graph with given probabilities
     graphGen(matrix, dim, p);
@@ -39,48 +48,44 @@ double triangles(vector<int> &matrix, vector<int> &output, int dim, float p)
     strassen(matrix, output, output, dim);
 
     // Add diagonal entries
-    double diagSum = 0;
+    double diagSum = 0.0;
     for (int i = 0; i < dim; i++)
     {
-        diagSum = diagSum + output[i * i];
+        diagSum = diagSum + (double)output[i * dim + i];
     }
-    double numTriangles = diagSum / 6;
+    double numTriangles = (double)diagSum / (double)divFactor;
     return numTriangles;
 }
 
 int main()
 {
-
     // Edit dims, probabilities, numTrials here
     int dim = 1024;
-    std::array<float, 5> probs = {0.01, 0.02, 0.03, 0.04, 0.05};
-    int numTrials = 5;
+    // std::array<float, 5> probs = {0.01, 0.02, 0.03, 0.04, 0.05};
+    double numTrials = 5;
 
     // Test values
-    dim = 10;
-    float p = 0.01;
+    dim = 1024;
+    std::array<float, 1> probs = {0.01};
+    numTrials = 1;
 
-    // Initialize adjacency matrix
+    // Initialize adjacency, output matrices
     vector<int> matrix;
     matrix.resize(dim * dim);
+    vector<int> output;
+    output.resize(dim * dim);
 
-    graphGen(matrix, dim, p);
-    print_mat(matrix, dim);
-    return 0;
-
-    /* Code to run triangles
     // Compute average number of triangles for given parameters
     for (int i = 0; i < probs.size(); i++)
     {
         double avg = 0;
         for (int j = 0; j < numTrials; j++)
         {
-            // avg += triangles(dim, p);
-            avg += 1;
+            avg += triangles(matrix, output, dim, probs[i]);
         }
         avg = avg / numTrials;
         cout << "Average number of triangles, probability " << probs[i] << ": " << avg << endl;
     }
 
-    */
+    return 0;
 }
